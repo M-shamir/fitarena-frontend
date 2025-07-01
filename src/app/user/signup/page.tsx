481ef4fe6b8a91466a7cb791fad54705c80b0627
M-shamir <mshamir732gmail.com>
2025-07-01 16:02:37 +0530

    feat: add Google OAuth integration for user authentication

--- src/app/user/signup/page.tsx
+++ src/app/user/signup/page.tsx
@@ -7,6 +7,8 @@ import { z } from "zod";
 import { useRouter } from 'next/navigation'
 import { ToastContainer, toast } from 'react-toastify';
 import Head from 'next/head';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import useAuthStore from '@/store/authStore';
 
 type FormData = {
   username: string;
@@ -80,7 +82,42 @@ export default function SignUp() {
     }
   };
 
+  const handleGoogleSuccess = async (credentialResponse: any) => {
+    console.log('Google response:', credentialResponse);
+    try {
+      useAuthStore.getState().setLoading(true);
+      const response = await api.post('user/auth/google/', {
+        token: credentialResponse.credential
+      });
+      
+      const result = response.data;
+      toast.success(result.message || 'Google sign-in success');
+      useAuthStore.getState().login({
+        id: result.user.id,
+        username: result.user.username,
+        email: result.user.email,
+        role: result.user.role,
+        // Include any other user fields you need
+      });
+      
+
+      router.push('/')
+
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || 'Google sign-in failed');
+    }
+    finally {
+     
+      useAuthStore.getState().setLoading(false);
+    }
+  };
+
+  const handleGoogleError = () => {
+    toast.error('Google sign-in failed');
+  }
+
   return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''}>
     <div className="min-h-screen bg-gray-900 flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8">
       <Head>
         <title>Sign Up | FitArena</title>
@@ -197,17 +234,18 @@ export default function SignUp() {
           </div>
 
           <div className="mt-6 grid grid-cols-2 gap-3">
-            <div>
-              <button
-                className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
-                onClick={() => console.log('Google sign-in')}
-              >
-                <svg className="h-5 w-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"/>
-                </svg>
-                Google
-              </button>
-            </div>
+          
+            <GoogleLogin
+                onSuccess={handleGoogleSuccess}
+                onError={handleGoogleError}
+                useOneTap
+                text="signup_with"
+                shape="rectangular"
+                size="large"
+                width="100%"
+                theme="filled_blue"
+              />
+           
 
             <div>
               <button
@@ -225,5 +263,6 @@ export default function SignUp() {
       </div>
       <ToastContainer />
     </div>
+    </GoogleOAuthProvider>
   );
 }
